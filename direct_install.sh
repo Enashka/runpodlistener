049b8e537673ce7333669861bf182381b1b63584
NicoHuve <nicohuve@gmail.com>
2025-03-12 13:44:11 +0100

    Add direct installation script

---
+++ direct_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Direct installation script for RunPod ComfyUI Image Sync
+
+# Download the installation script
+wget https://raw.githubusercontent.com/Enashka/runpodlistener/main/install_on_runpod.sh
+
+# Make it executable
+chmod +x install_on_runpod.sh
+
+# Run the installation script
+sudo ./install_on_runpod.sh 
